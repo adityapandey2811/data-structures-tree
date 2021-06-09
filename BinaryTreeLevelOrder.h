12ebbd1ce02d71164ab5837bcbb7b2f29e048262
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-06-09 17:33:07 +0530

    Create BinaryTreeLevelOrder.h

---
+++ BinaryTreeLevelOrder.h
@@ -0,0 +1,96 @@
+#ifndef bt_h
+#define bt_h
+
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct binary{
+    int data;
+    struct binary *left,*right;
+}bt;
+typedef struct qm{
+    bt *treenode;
+    struct qm *next;
+}queueMaker;
+typedef struct qtn{
+    queueMaker *front,*rear;
+}queueTreeNode;
+queueTreeNode* enqueueTreeNode(queueTreeNode *head, bt *n){
+    queueMaker *temp = NULL;
+    temp = (queueMaker*)malloc(sizeof(queueMaker));
+    temp->treenode = n;
+    temp->next = NULL;
+    if(head == NULL){
+        head = (queueTreeNode*)malloc(sizeof(queueTreeNode));;
+        head->front = head->rear = temp;
+    }
+    else{
+        head->rear->next = temp;
+        head->rear = temp;
+    }
+    return head;
+}
+bt* dequeTreeNode(queueTreeNode **head){
+    bt *newnode = NULL;
+    if((*head)!=NULL){
+        newnode = (*head)->front->treenode;
+        (*head)->front = (*head)->front->next;
+    }
+    if((*head)->front == NULL)
+        (*head) = NULL;
+    return newnode;
+}
+void inorder(bt *root){
+    if(root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ",root->data);
+    inorder(root->right);
+}
+void preorder(bt *root){
+    if(root == NULL)
+        return;
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(bt *root){
+    if(root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
+bt* constructbt(){
+    bt *temp = NULL;
+    queueTreeNode *head = NULL;
+    temp = (bt*)malloc(sizeof(bt));
+    printf("Enter root node data: ");
+    scanf("%d",&temp->data);
+    temp->left = temp->right = NULL;
+    head = enqueueTreeNode(head,temp);
+    while(head!=NULL){
+        bt *parent = dequeTreeNode(&head),*leftc = NULL, *rightc = NULL;
+        int n;
+        printf("Enter left of %d: ",parent->data);
+        scanf("%d",&n);
+        if(n!=-1){
+            leftc = (bt*)malloc(sizeof(bt));
+            leftc->data = n;
+            leftc->left = leftc->right = NULL;
+            parent->left = leftc;
+            head = enqueueTreeNode(head,leftc);
+        }
+        printf("Enter right of %d: ",parent->data);
+        scanf("%d",&n);
+        if(n!=-1){
+            rightc = (bt*)malloc(sizeof(bt));
+            rightc->data = n;
+            rightc->left = rightc->right = NULL;
+            parent->right = rightc;
+            head = enqueueTreeNode(head,rightc);
+        }
+    }
+    return temp;
+}
+
+#endif
