cb573aa90f31da54924b36d853a8fdc2dbfccf3c
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-06-10 18:44:52 +0530

    Update BinaryTreeLevelOrder.h

--- BinaryTreeLevelOrder.h
+++ BinaryTreeLevelOrder.h
@@ -48,7 +48,7 @@ void displayL(bt *root){
     queueTreeNode *head = NULL;
     int n;
     if(root == NULL){
-        printf("Empty!!");
+        printf("Empty!!!");
         return;
     }
     head = enqueueTreeNode(head,root);
@@ -113,6 +113,23 @@ bt* constructbt(){
         }
     }
     return temp;
+}
+void deleteTree(bt **root){
+    queueTreeNode *head = NULL;
+    if((*root) == NULL){
+        printf("Already Empty!!!\n");
+        return;
+    }
+    head = enqueueTreeNode(head,(*root));
+    while(head!=NULL){
+        bt *parent = dequeTreeNode(&head);
+        if(parent->left != NULL)
+            head = enqueueTreeNode(head,parent->left);
+        if(parent->right != NULL)
+            head = enqueueTreeNode(head,parent->right);
+        free(parent);
+    }
+    (*root) = NULL;
 }
 bt *insertInTree(bt *root, int n){
     queueTreeNode *head = NULL;
