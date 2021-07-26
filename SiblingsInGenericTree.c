3213218028fa9002b71269e429be808030a7e6f0
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-07-26 16:31:44 +0530

    Create SiblingsInGenericTree.c

---
+++ SiblingsInGenericTree.c
@@ -0,0 +1,15 @@
+//Structure Type
+typedef struct genericTree{
+    int data;
+    struct genericTree *nextSibling, *firstChild;
+}gentre;
+
+//Calculation of number of siblings
+int noOfSiblings(gentre *root){
+    int i = 0,j,k,n;
+    while(root != NULL){
+        root = root->nextSibling;
+        i++;
+    }
+    return i;
+}
