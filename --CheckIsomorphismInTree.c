dda6b507627de8323e659127ecac29044a016290
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-10-29 08:43:30 +0530

    Update --CheckIsomorphismInTree.c

--- --CheckIsomorphismInTree.c
+++ --CheckIsomorphismInTree.c
@@ -17,3 +17,26 @@ int main(){
         printf("Not Isomorphic\n");
     return 0;
 }
+
+//Updated solution
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"btl.h"
+int checkIsomorphic(bt *root1, bt *root2){
+    if(root1 == NULL && root2 == NULL)
+        return 1;
+    if(root1 == NULL || root2 == NULL)
+        return 0;
+    return checkIsomorphic(root1->left, root2->left) && checkIsomorphic(root1->right, root2->right);
+}
+int main(){
+    bt *root1 = constructbt(), *root2 = constructbt();
+    int i = checkIsomorphic(root1,root2);
+    if(i == 1)
+        printf("Isomorphic\n");
+    else
+        printf("Not Isomorphic\n");
+    return 0;
+}
+//1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1 
