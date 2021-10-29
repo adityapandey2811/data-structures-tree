35f4cda38ca1829a9760a8430833c04ff7db0dbf
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-10-29 20:27:31 +0530

    Create --EF--BST--CheckIfTreeIsBST.c

---
+++ --EF--BST--CheckIfTreeIsBST.c
@@ -0,0 +1,28 @@
+//Optimized approach to check BST
+#include<stdio.h>
+#include<stdlib.h>
+#include"btl.h"
+int checkBST(bt *root){
+    int i = 0;
+    if(root == NULL)
+        return 1;
+    checkBST(root->left);
+    checkBST(root->right);
+    if(root->left == NULL || root->left->data < root->data)
+        i++;
+    if(root->right == NULL || root->right->data > root->data)
+        i++;
+    if(i == 2)
+        return 1;
+    return 0;
+}
+int main(){
+    bt *root = constructbt();
+    int i = checkBST(root);
+    if(i == 1)
+        printf("BST\n");
+    else
+        printf("Not BST\n");
+    return 0;
+}
+// 25 20 36 10 22 30 40 5 12 -1 -1 28 -1 38 48 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
