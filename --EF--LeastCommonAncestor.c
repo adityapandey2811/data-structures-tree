3ccc2febd423d78263be6aefabde295a3ca5f4da
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-10-29 08:30:39 +0530

    Update --EF--LeastCommonAncestor.c

--- --EF--LeastCommonAncestor.c
+++ --EF--LeastCommonAncestor.c
@@ -27,3 +27,49 @@ int main(){
     printf("%d\n",res);
     return 0;
 }
+
+
+
+
+//Same Technique little update
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"btl.h"
+int lca(bt *root, int a, int b, int *res, int *init){
+    int l, r;
+    if(root == NULL)
+        return 0;
+    l = lca(root->left,a,b,res,init);
+    r = lca(root->right,a,b,res,init);
+    if(root->data == a){
+        *init = *init + 1;
+        return 1;
+    }
+    if(root->data == b){
+        *init = *init + 1;
+        return 1;
+    }
+    if(l == 1 && r == 1){
+        *res = root->data;
+        *init = 0;
+        return 0;
+    }
+    if((l == 1 || r == 1) && *init == 2){
+        *res = root->data;
+        *init = 0;
+        return 0;
+    }
+    if(l == 1 || r == 1)
+        return 1;
+    return 0;
+}
+int main(){
+    int res = 0, init = 0;
+    bt *root = constructbt();
+    inorder(root);
+    printf("\n");
+    lca(root, 6, 9, &res, &init);
+    printf("LCA: %d\n",res);
+    return 0;
+}
