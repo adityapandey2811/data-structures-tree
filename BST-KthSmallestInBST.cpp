d86b86807ad94ea475be25ec6ff0efbdc0d85bb7
Aditya Pandey <2811adityapandey@gmail.com>
2023-04-12 23:44:32 +0530

    Update BST-KthSmallestInBST.cpp

--- BST-KthSmallestInBST.cpp
+++ BST-KthSmallestInBST.cpp
@@ -1,3 +1,24 @@
+// Optimal Solution
+void inorder(TreeNode* root, int *res, int *k) {
+    if(!root) return;
+    if(*k != 0) {
+        inorder(root->left,res,k);
+    }
+    if(*k != 0) {
+        *k = *k-1;
+        *res = root->val;
+    }
+    if(*k != 0) {
+        inorder(root->right,res,k);
+    }
+}
+int kthSmallest(TreeNode* root, int k) {
+    int res = 0;
+    inorder(root, &res, &k);
+    return res;
+}
+
+// Bad Solution
 void inorder(TreeNode* root, int k, priority_queue <int, vector<int>, greater<int> >& minHeap) {
     if(root == NULL) return;
     inorder(root->left, k, minHeap);
